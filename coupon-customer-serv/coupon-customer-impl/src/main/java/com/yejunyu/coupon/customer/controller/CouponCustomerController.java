9b783d220beea5103d2a67ed99f32796752fb5b6
yjy <yyyejunyu@gmail.com>
2023-09-01 17:36:55 +0800

    feat: sentinel pom

--- coupon-customer-serv/coupon-customer-impl/src/main/java/com/yejunyu/coupon/customer/controller/CouponCustomerController.java
+++ coupon-customer-serv/coupon-customer-impl/src/main/java/com/yejunyu/coupon/customer/controller/CouponCustomerController.java
@@ -9,6 +9,8 @@ import com.yejunyu.coupon.customer.dao.entity.Coupon;
 import com.yejunyu.coupon.customer.service.CouponCustomerService;
 import com.yejunyu.coupon.template.api.beans.CouponInfo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -23,13 +25,22 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/customer")
+// 监听配置变动然后推送
+@RefreshScope
 public class CouponCustomerController {
     @Resource
     private CouponCustomerService customerService;
 
+    @Value("${disableCouponRequest:false}")
+    private Boolean disableCoupon;
+
     @PostMapping("/requestCoupon")
     public Coupon requestCoupon(@Valid @RequestBody CouponReq request) {
         log.info("CouponCustomerController#requestCoupon request={}", request);
+        if (disableCoupon) {
+            log.info("暂停领取优惠券");
+            return null;
+        }
         return customerService.requestCoupon(request);
     }
 
